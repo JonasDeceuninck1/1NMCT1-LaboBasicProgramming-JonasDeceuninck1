b347c640e5ef7696792a812ba7aa83ea9dc200d3
JonasDeceuninck1 <jonas.deceuninck@student.howest.be>
2017-09-26 09:59:20 +0200

    Initial commit

---
+++ Oef01.py
@@ -0,0 +1,11 @@
+#dit is commentaar
+# labo_intro Oef1
+naam = input("vul hier u naam in")
+leeftijd = int(input("wat is u leeftijd?"))
+voornaam = "Jonas"
+leeftijd += 10
+#print("hello world" , voornaam , naam)
+
+print("welkom {0}. jouw naam is {1} u bent {2: .2f} jaar oud".format(voornaam,naam, leeftijd))
+print("welkom {0}. jouw naam is {1} jij bent {2: .2f} jaar oud".format(voornaam,naam, leeftijd))
+print("welkom {0}. jouw naam is {1} u bent {2: .2f} jaar oud".format(voornaam,naam, leeftijd))
